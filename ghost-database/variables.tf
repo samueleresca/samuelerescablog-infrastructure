eee7adae325a2e0e17d3c0524fd373b2dd1f0249
Samuele Resca <samuele.resca@gmail.com>
2020-02-10 13:02:41 +0000

    initial commit

---
+++ ghost-database/variables.tf
@@ -0,0 +1,5 @@
+variable "name" { type = "string" }
+variable "db_name" { type = "string" }
+variable "db_user" { type = "string" }
+variable "db_pass" { type = "string" }
+variable "security_groups" { type = "list" }
